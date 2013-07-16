98c80e16c072d142fe7db189fddb2dca3a35f877
Harold Asiimwe <harryasiimwe@gmail.com>
2013-07-16 13:15:04 +0300

    Third Commit with Tests

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Poll, Choice
@@ -11,8 +12,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last published polls """
-        return Poll.objects.order_by('-pub_date')[:5]
+        """
+            Return the last five published polls (not including those set to be
+                published in the near future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -31,7 +37,10 @@ def vote(request, poll_id):
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         #Redisplay the poll voting form
-        return render(render, 'polls/detail.html', {'poll':p, 'error_message': "You didn't select a choice"})
+        return render(request,'polls/detail.html', {
+            'poll':p, 
+            'error_message': "You didn't select a choice"
+            })
     else:
         selected_choice.votes += 1
         selected_choice.save()    
